a1cfe6dd8e71102c44da9b0d94de96e5519682c5
Ramifus <ramifus926@gmail.com>
2026-09-22 20:37:51 -0400

    sdfasdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://boda-samuel-y-mary-2026.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   }
